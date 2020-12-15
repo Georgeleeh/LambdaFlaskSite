c8e4b73a3d9780d6a00ae17414b58580057d8d75
Georgeleeh <me+code@georgeleeh.com>
2020-12-15 15:58:31 +0000

    implemented putting posts and tags

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app, db
+from app import app, db_posts, db_tags
 
 from flask import render_template
 
@@ -6,19 +6,43 @@ from botocore.exceptions import ClientError
 
 
 
-def get_blog_post(timestamp, dynamodb=db):
+def get_blog_post(timestamp):
     try:
-        response = dynamodb.get_item(Key={'timestamp': timestamp})
+        response = db_posts.get_item(Key={'timestamp': timestamp})
     except ClientError as e:
         print(e.response['Error']['Message'])
     else:
-        r = response['Item']
         return response['Item']
+
+def put_blog_post(timestamp, title, content, tags):
+    tag_set = set([t.strip() for t in tags.split(',')])
+    print(tag_set)
+    item = {
+        'timestamp' : timestamp,
+        'title' : title,
+        'content' : content,
+        'tags' : tag_set
+    }
+
+    response = db_posts.put_item(Item=item)
+    response_code = response['ResponseMetadata']['HTTPStatusCode']
+
+    if response_code == 200:
+        for tag in tag_set:
+            db_tags.update_item(
+                Key={'tag' : tag},
+                UpdateExpression=f'ADD posts :p',
+                ExpressionAttributeValues={':p' : set([timestamp])}
+                )
+    else:
+        raise Exception(f'Failed to create post: {response}')
+
+    return response
 
 
 
 @app.route('/')
 def home():
-    response = get_blog_post(1234)
-    print(response['timestamp'])
+    response = put_blog_post(4321, 'Second Post', "He's back, putting posts again", 'test, put')
+    print(response)
     return render_template('home.html')
