072a3a5dadfda68486dcfc18e903ef1b005b5585
Georgeleeh <me+code@georgeleeh.com>
2020-12-15 14:57:07 +0000

    added files from previous site and changed DB interface

--- app/routes.py
+++ app/routes.py
@@ -8,18 +8,17 @@ from botocore.exceptions import ClientError
 
 def get_blog_post(timestamp, dynamodb=db):
     try:
-        response = dynamodb.get_item(TableName='georgeleeh_blog_posts', Key={'timestamp': {'N': str(timestamp)}})
+        response = dynamodb.get_item(Key={'timestamp': timestamp})
     except ClientError as e:
         print(e.response['Error']['Message'])
     else:
+        r = response['Item']
         return response['Item']
 
 
 
 @app.route('/')
-@app.route('/index')
-def hello_world():
+def home():
     response = get_blog_post(1234)
-    print(response)
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    print(response['timestamp'])
+    return render_template('home.html')
